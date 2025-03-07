83c34a64f098a012e9bfb46e5b831c7e9da46617
Danilo Rodrigues <zidalo@gmail.com>
2025-03-07 18:28:45 -0300

    feat: :heavy_plus_sign: Add supabase_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://aroenqrnmelxtqarcuyw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyb2VucXJubWVseHRxYXJjdXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzODE0NTQsImV4cCI6MjA1Njk1NzQ1NH0.8NahGQDUoR4uTlio6VezRP05nHCOmN3Haomw4PFGu00',
+  );
+
   runApp(const MainApp());
 }
 
